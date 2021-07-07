f6808e95548dba6fc3bf20e88f9c9e47c4f7db6f
ogger77 <vud@kean.edu>
2021-07-07 14:26:10 -0400

    minor fix

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,7 @@ const instance = axios.create({
   // The API cloud function
   baseURL: "https://us-central1-clone-a7cff.cloudfunctions.net/api",
   // dev http://localhost:5001/clone-a7cff/us-central1/api
+  // prod https://us-central1-clone-a7cff.cloudfunctions.net/api
 });
 
 export default instance;
